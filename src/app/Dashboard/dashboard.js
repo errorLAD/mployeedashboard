744622801720bde8a95cb55a88188aad80b3a9fd
Abhishek Mishra <ab.mishra@yahoo.com>
2024-05-10 17:03:51 +0530

    Update dashboard.js

--- src/app/Dashboard/dashboard.js
+++ src/app/Dashboard/dashboard.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Navbar from "./ui/navbar";
-import Sidenav from "./ui/sidenav";
-import Section from "./ui/section/section";
+import Navbar from "./navbar";
+import Sidenav from "./sidenav";
+import Section from "./section";
 
 export default function Dashboard() {
   return (
-    <div class="container mx-auto px-4 ">
+    <div className="container mx-auto px-4 ">
       <Navbar />
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
       <div className="h-32 rounded-lg bg-gray-200">
